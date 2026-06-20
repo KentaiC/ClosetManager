bdf281691ea9907170db5c65f2c5ed6ba0e99179
KentaiC <KentaiC@users.noreply.github.com>
2026-06-20 15:15:16 -0400

    Initial Commit

---
+++ ClosetManager/ClosetManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetManagerApp.swift
+//  ClosetManager
+//
+//  Created by Evan Li on 2026-06-20.
+//
+
+import SwiftUI
+
+@main
+struct ClosetManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
